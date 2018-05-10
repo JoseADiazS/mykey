0e58d655286da84f60c8619f71f7f289f25dda96
andres <asanabr34637@universidadean.edu.co>
2018-05-09 20:31:34 -0500

    cambio nombre

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <div class="navbar-header">
-      <a class="navbar-brand" href="#">WebSiteName</a>
+      <a class="navbar-brand" href="#">My Key</a>
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href="index.php">Home</a></li>
