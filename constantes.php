cf4c45738f64c2698380cecf689205edaf632343
Jose <jdiazseg9611@universidadean.edu.co>
2018-06-05 13:39:12 -0500

    inicio de documentacion

--- constantes.php
+++ constantes.php
@@ -1,4 +1,7 @@
 <?php
+    /**
+    *Variables que seran utilizadas en toda la pagina
+    */
     define("TITULO_PAG", "My Key");
     define("AUTHOR","Jose Diaz, Camilo Ceballos, Andres Sanabria");
 ?>
--- header.php
+++ header.php
@@ -2,11 +2,15 @@
 <html lang="en">
 
 <head>
+   <!--Nombre de la pagina -->
     <title>My Key</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Relacion de estilos con bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <!-- Relacion de scripts con jQuery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- Relacion de scripts con bootstrap -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <meta http-equiv="X-UA-Compatible" content="ie-edge">
 </head>
