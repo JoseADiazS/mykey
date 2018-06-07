9c1ce4982a869d80fccf000e4abd7186027cf24a
Jose <jdiazseg9611@universidadean.edu.co>
2018-06-06 23:56:28 -0500

    Documentacion

--- solicitar-servicio.php
+++ solicitar-servicio.php
@@ -10,27 +10,31 @@ require('header.php')
 <head>
     <meta charset="UTF-8">
     <title>Inicio</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <div id="map"></div>
+   <!-- div mapa donde se ubicara el mapa -->
+    <div id="map" class="row"></div>
+    <!-- style de tamaño predeterminado para el mapa -->
     <style>
         #map {
             height: 400px;
             width: 400px;
         }
-
     </style>
-
+    <!-- script con el llamado a la api de google maps, utilizando la key obtenida para el proyecto -->
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAPxUu1AacTervEEX5JzYYbX5V0LzKHEH4&callback=initMap"></script>
+    <!-- script con el contenido del inicio y control del mapa -->
     <script src="js/script2.js" type="text/javascript"></script>
 
-
+    <!--  contenido del body restante en la pagina-->
     <div class="container">
         <div class="row centered-form">
             <div class="col-xs-12 col-sm-8 col-md-4 col-sm-offset-2 col-md-offset-4">
                 <div class="panel panel-default">
                     <div class="panel-heading">
+                       <!-- Panel de titulo para la seleccion del servicio -->
                         <h3 class="panel-title">Bienvenido seleccione su servicio</h3>
                     </div>
                     <div class="panel-body">
@@ -50,6 +54,7 @@ require('header.php')
                                     <option>Arreglo cerradura para autos</option>
                                 </select>
                                 <br>
+                                <!-- boton para completar la solicitud del servicio -->
                                 <button type="submit" class="btn btn-info btn-block">Pedir servicio</button>
                             </div>
 
